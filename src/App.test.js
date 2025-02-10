9b148bc63709dba2d8e50d1fc277a0230da33e66
santhosh1995durai <santhosh@Santhosh>
2025-02-10 18:30:08 +0530

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,28 @@
+import { act } from 'react'
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  act(() => {
+    render(<App />);
+  });
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('renders learn react link', () => {
+  act(() => {
+    render(<App />);
+  });
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  act(() => {
+    render(<App />);
+  });
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
